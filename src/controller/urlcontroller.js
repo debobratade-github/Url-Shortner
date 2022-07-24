df844d35358cf0b13f396d7923a46ae17c074abd
debobratade-github <debobratade@gmail.com>
2022-07-24 20:40:50 +0530

    Url Shortner

--- src/controller/urlcontroller.js
+++ src/controller/urlcontroller.js
@@ -31,8 +31,6 @@ redisClient.on("connect", async function () {
   console.log("Connected to Redis..");
 });
 
-//1. connect to the server
-//2. use the commands :
 
 //Connection setup for redis
 const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
@@ -44,27 +42,32 @@ const createurl = async function (req, res) {
   try {
     let url = req.body
 
-    // first we check req.body empty or not
+    // first we check req.body empty or not.
     if (!isValidRequest(url)) return res.status(400).send({ status: false, message: "please enter data in body" })
-    // we check here if any put empty string or extra space
+
+    // we check here if any put empty string or extra space.
     if (!isValidValue(url.longUrl)) return res.status(400).send({ status: false, message: "please enter valid input" })
-    //  we can check  here  url in valid format or not
+
+    //  we check here url is in valid format or not.
     if (!urlvalidation(url.longUrl)) return res.status(400).send({ status: false, message: "please enter valid url" })
 
+    // Ensure that user input only longUrl.
     let val = (Object.keys(url))
     if (!(val.length == 1 && val[0] == "longUrl"))
       return res.status(400).send({ status: false, message: "Input only longUrl" })
 
-
+    // First check in cache for the document if present then return from here.
     let cacheurl = await GET_ASYNC(`${url.longUrl}`)
     if (cacheurl) {
       let abc = JSON.parse(cacheurl)
       return res.status(200).send({ status: true, data: abc })
     }
 
+    // Then look into database.  
     else if (!cacheurl) {
       let sameurl = await urlModel.findOne({ longUrl: url.longUrl }).select({ _id: 0, updatedAt: 0, createdAt: 0, __v: 0 })
 
+      // If not found then create the document in database and set in cache.
       if (sameurl == null) {
         let urlCode = shortid.generate().toLowerCase()
         let baseurl = "http://localhost:3000/"
@@ -77,6 +80,7 @@ const createurl = async function (req, res) {
         return res.status(201).send({ status: true, data: urldata })
       }
 
+      // If found in database then return the data and set in cache. 
       await SET_ASYNC(`${url.longUrl}`, JSON.stringify(sameurl))
       return res.status(200).send({ status: true, data: sameurl })
     }
@@ -90,22 +94,34 @@ const geturl = async (req, res) => {
   try {
     let codeurl = req.params.urlCode
 
+    // Check input is vaild urlcode or not. 
+    if ((shortid.isValid(codeurl) == false) || (codeurl.length <= 7) || (codeurl.length >= 14)) {
+      return res.status(400).send({ status: false, message: "please enter valid urlCode" })
+    }
 
-    if ((shortid.isValid(codeurl) == false) || (codeurl.length <= 7) || (codeurl.length >= 14)) return res.status(400).send({ status: false, message: "please enter valid urlCode" })
-
-    let urlcode1 = await urlModel.findOne({ urlCode: codeurl })
-    if (!urlcode1) return res.status(404).send({ status: false, message: "unable to find url" })
-
+    // Then look into the cache for the specific data.  
     let cacheurl = await GET_ASYNC(`${codeurl}`)
     let data = JSON.parse(cacheurl)
 
+    // If found the data then return and redirect with the longUrl. 
     if (cacheurl) {
       return res.status(302).redirect(data.longUrl)
 
-    } else {
+
+    }
+
+    // Else look into database for the data and set the data in cache for next search. 
+    else {
       let findurlcode = await urlModel.findOne({ urlCode: codeurl }).select({ urlCode: 0, _id: 0 });
-      await SET_ASYNC(`${codeurl}`, JSON.stringify(findurlcode))
-      return res.status(302).redirect(findurlcode.longUrl);
+
+      if (findurlcode != null) {
+        await SET_ASYNC(`${codeurl}`, JSON.stringify(findurlcode))
+        return res.status(302).redirect(findurlcode.longUrl);
+      }
+      // If didn't find the data in database.
+      else {
+        return res.status(404).send({ status: false, message: "unable to find url" })
+      }
     }
 
   } catch (err) { return res.status(500).send({ status: false, msg: err.message }) }
